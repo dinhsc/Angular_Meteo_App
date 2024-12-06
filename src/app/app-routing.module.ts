a59bb23b07c39cdee7ecc3ac19d55bdf1ca73196
dinhsc <dinhc692@gmail.com>
2024-12-06 20:03:10 +0100

    building other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CitiesComponent } from './cities/cities.component';
+import { MapComponent } from './map/map.component';
+import { SettingsComponent } from './settings/settings.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'cities', component: CitiesComponent },
+  { path: 'maps', component: MapComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
